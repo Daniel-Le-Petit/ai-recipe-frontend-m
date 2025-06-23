e1059bee901fa884068031b81b66d4876601d5ea
Daniel Le Petit <dlepetit.maa@gmal.com>
2025-06-23 09:19:15 +0200

    Initial commit fontend-m

--- creategit.sh
+++ creategit.sh
@@ -1,4 +1,4 @@
-cd /mnt/c:/Users/AIFinesHerbes/AIFB/frontend-m
+cd /mnt/c/Users/AIFinesHerbes/AIFB/frontend-m
 git init
 git remote add origin https://github.com/Daniel-Le-Petit/frontend-m.git
 git add .
